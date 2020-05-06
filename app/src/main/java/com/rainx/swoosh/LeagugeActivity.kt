ef878d0cd66a1f73386e47747583318ff89eb2c1
cvcharan51 <cvcharan51@gmail.com>
2020-05-06 10:26:46 +0530

    Added league layout
    Added

---
+++ app/src/main/java/com/rainx/swoosh/LeagugeActivity.kt
@@ -0,0 +1,12 @@
+package com.rainx.swoosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LeagugeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_leaguge)
+    }
+}
